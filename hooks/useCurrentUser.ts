fa9795f186084041ba0354c86218b6a4ad2500e8
Pulkit0906 <pulkitjain10000@gmail.com>
2023-12-10 11:58:20 +0530

    back

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -3,11 +3,10 @@ import useSWR from "swr";
 import fetcher from "@/libs/fetcher";
 
 const useCurrentUser = () => {
-    const { data, error, isLoading,image, mutate } = useSWR('/api/current', fetcher)
+    const { data, error, isLoading, mutate } = useSWR('/api/current', fetcher)
 
     return {
         data,
-        image,
         error,
         isLoading,
         mutate
