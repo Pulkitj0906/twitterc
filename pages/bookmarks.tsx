a9327ffb7fa33b5ed9c455893e38cb011fda9480
Pulkit0906 <pulkitjain10000@gmail.com>
2024-01-14 10:40:20 +0530

    try#2

--- pages/bookmarks.tsx
+++ pages/bookmarks.tsx
@@ -5,29 +5,29 @@ import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 
-export async function getServerSideProps(context: NextPageContext) {
-  try {
-    const session = await getSession(context);
+// export async function getServerSideProps(context: NextPageContext) {
+//   try {
+//     const session = await getSession(context);
 
-    if (!session) {
-      return {
-        redirect: {
-          destination: '/',
-          permanent: false,
-        }
-      };
-    }
+//     if (!session) {
+//       return {
+//         redirect: {
+//           destination: '/',
+//           permanent: false,
+//         }
+//       };
+//     }
 
-    return {
-      props: {
-        session
-      }
-    };
-  } catch (error) {
-    console.error("Error in getServerSideProps:", error);
-    throw error;
-  }
-}
+//     return {
+//       props: {
+//         session
+//       }
+//     };
+//   } catch (error) {
+//     console.error("Error in getServerSideProps:", error);
+//     throw error;
+//   }
+// }
 
 const Bookmarks = () => {
   return ( 
